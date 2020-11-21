63646c3cb4b704a6fb1822e484f7dcd1028beb8d
kumapanda723 <kumapanda723@icloud.com>
2020-11-21 20:27:48 +0900

    client登録時のバリデーション追加・登録機能の実装

--- app/Http/Controllers/AddclientsController.php
+++ app/Http/Controllers/AddclientsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Client;
 
 class AddclientsController extends Controller
 {
@@ -19,14 +20,15 @@ class AddclientsController extends Controller
     public function register(Request $request)
     {
         $request->validate([
-            'client_name' => 'required',
+            'client_name' => 'required||unique:clients,client_name',
             'capital_amount' => 'required||digits_between:1,10',
             'annual_sales_1' => 'digits_between:1,13',
             'annual_sales_2' => 'digits_between:1,13',
             'annual_sales_3' => 'digits_between:1,13',
         ]);
         
+        Client::create($request->all());
         
-        return;
+        return view('add_clients');
     }
 }
