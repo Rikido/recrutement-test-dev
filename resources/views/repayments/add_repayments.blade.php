95accb10ef7e2a860dc82724da45ee46a626780f
kumapanda723 <kumapanda723@icloud.com>
2020-11-25 23:49:35 +0900

    credit_minus算出ロジックの実装

--- resources/views/repayments/add_repayments.blade.php
+++ resources/views/repayments/add_repayments.blade.php
@@ -58,6 +58,65 @@
           <input type="checkbox" name="delay_flag" class="delay-text" value="no"> なし
         </div>
       </div>
+  
+      <div class="credit-minus">
+        <label for="credit_minus"></label>
+        <div class="input-form">
+
+          @if($trade->transaction_amount <= 1000000 && $trade->months_of_term <= 6)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-20">
+          @elseif($trade->transaction_amount <= 1000000 && $trade->months_of_term > 6 && $trade->months_of_term <= 12)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-30">
+          @elseif($trade->transaction_amount <= 1000000 && $trade->months_of_term > 12 && $trade->months_of_term <= 36)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-40">
+          @elseif($trade->transaction_amount <= 1000000 && $trade->months_of_term > 36 && $trade->months_of_term <= 60)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-50">
+          @elseif($trade->transaction_amount <= 1000000 && $trade->months_of_term > 60 && $trade->months_of_term <= 84)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-60">
+          @elseif($trade->transaction_amount > 1000000 && $trade->transaction_amount <= 10000000 && $trade->months_of_term <= 6)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-5">
+          @elseif($trade->transaction_amount > 1000000 && $trade->transaction_amount <= 10000000 && $trade->months_of_term > 6 && $trade->months_of_term <= 12)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-10">
+          @elseif($trade->transaction_amount > 1000000 && $trade->transaction_amount <= 10000000 && $trade->months_of_term > 12 && $trade->months_of_term <= 36)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-20">
+          @elseif($trade->transaction_amount > 1000000 && $trade->transaction_amount <= 10000000 && $trade->months_of_term > 36 && $trade->months_of_term <= 60)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-30">
+          @elseif($trade->transaction_amount > 1000000 && $trade->transaction_amount <= 10000000 && $trade->months_of_term > 60 && $trade->months_of_term <= 84)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-40">
+          @elseif($trade->transaction_amount > 10000000 && $trade->transaction_amount <= 50000000 && $trade->months_of_term <= 6)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-3">
+          @elseif($trade->transaction_amount > 10000000 && $trade->transaction_amount <= 50000000 && $trade->months_of_term > 6 && $trade->months_of_term <= 12)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-5">
+          @elseif($trade->transaction_amount > 10000000 && $trade->transaction_amount <= 50000000 && $trade->months_of_term > 12 && $trade->months_of_term <= 36)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-10">
+          @elseif($trade->transaction_amount > 10000000 && $trade->transaction_amount <= 50000000 && $trade->months_of_term > 36 && $trade->months_of_term <= 60)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-20">
+          @elseif($trade->transaction_amount > 10000000 && $trade->transaction_amount <= 50000000 && $trade->months_of_term > 60 && $trade->months_of_term <= 84)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-30">
+          @elseif($trade->transaction_amount > 50000000 && $trade->transaction_amount <= 100000000 && $trade->months_of_term <= 6)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-1">
+          @elseif($trade->transaction_amount > 50000000 && $trade->transaction_amount <= 100000000 && $trade->months_of_term > 6 && $trade->months_of_term <= 12)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-3">
+          @elseif($trade->transaction_amount > 50000000 && $trade->transaction_amount <= 100000000 && $trade->months_of_term > 12 && $trade->months_of_term <= 36)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-5">
+          @elseif($trade->transaction_amount > 50000000 && $trade->transaction_amount <= 100000000 && $trade->months_of_term > 36 && $trade->months_of_term <= 60)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-10">
+          @elseif($trade->transaction_amount > 50000000 && $trade->transaction_amount <= 100000000 && $trade->months_of_term > 60 && $trade->months_of_term <= 84)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-20">
+          @elseif($trade->transaction_amount > 100000000 && $trade->transaction_amount <= 1000000000 && $trade->months_of_term <= 6)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="0">
+          @elseif($trade->transaction_amount > 100000000 && $trade->transaction_amount <= 1000000000 && $trade->months_of_term > 6 && $trade->months_of_term <= 12)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-1">
+          @elseif($trade->transaction_amount > 100000000 && $trade->transaction_amount <= 1000000000 && $trade->months_of_term > 12 && $trade->months_of_term <= 36)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-3">
+          @elseif($trade->transaction_amount > 100000000 && $trade->transaction_amount <= 1000000000 && $trade->months_of_term > 36 && $trade->months_of_term <= 60)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-5">
+          @elseif($trade->transaction_amount > 100000000 && $trade->transaction_amount <= 1000000000 && $trade->months_of_term > 60 && $trade->months_of_term <= 84)
+            <input type="hidden" name="credit_minus" class="credit-minus-text" placeholder="信用スコア減点" value="-10">
+          @endif
+
+        </div>
+      </div>
 
       <button type="submit" class="submit-btn">登録</button>
     </from>
