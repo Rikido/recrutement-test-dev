c32a2cf72d54901dae1bada2b5fe7b76ead5bdf8
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-03-03 12:16:49 +0900

    seedファイルの設定、groupのfactoryファイルを作成

---
+++ database/factories/GroupFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+use App\Group;
+use Faker\Generator as Faker;
+
+$factory->define(Group::class, function (Faker $faker) {
+    return [
+        'group_name' => $faker->unique()->safeColorName,
+    ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        // userデータ登録
+        factory(App\User::class, 60)
+            ->create();
+
+        // 登録した商品データを全抽出
+        $users = App\User::all();
+
+        factory(App\Group::class, 10)
+            ->create()
+            ->each(function ($group) use ($users) {
+                $group->users()->attach(
+                    $users->random(rand(4,6))->pluck('id')->toArray() // 4～6個のuserをorderにランダムに紐づけ
+                );
+            });
     }
 }
