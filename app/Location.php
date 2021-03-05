a862032962fb05ad3f2b9672adf3cfd98a7dabc2
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-03-05 14:27:05 +0900

    Resource_stockモデルのリレーションの設定

--- app/Location.php
+++ app/Location.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
-    //
+    /**
+     *  Resource_stockモデルにリレーションの設定
+     */
+    public function resource_stocks()
+    {
+        return $this_>hasMany('App\Resource_stock');
+    }
 }
--- app/Resource_stock.php
+++ app/Resource_stock.php
@@ -6,5 +6,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resource_stock extends Model
 {
-    //
+    /**
+     *  このResource_stockを所有するLocationを取得
+     */
+    public function location()
+    {
+        return $this->belongsTo('App\Location');
+    }
+
+    /**
+     *  このResource_stockを所有するResourceを取得
+     */
+    public function resource()
+    {
+        return $this->belongsTo('App\Resource');
+    }
 }
