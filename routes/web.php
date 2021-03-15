eb5791592733ce4ae33c2adf8e631bba0cddd0ee
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-03-15 16:08:30 +0900

    ルーティングの設定

--- routes/web.php
+++ routes/web.php
@@ -7,16 +7,24 @@
 |
 | Here is where you can register web routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
+| contains the 'web' middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
+
+Route::get('/', 'ProjectsController@index');
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/groups', 'GroupController@index');
+Route::get('/groups', 'GroupsController@index');
+
+Route::get('/projects', 'ProjectsController@index');
+
+Route::get('/projects/create', 'ProjectsController@create');
+Route::post('/projects/create', 'ProjectsController@createStore');
+
+Route::get('/projects/confirm', 'ProjectsController@confirm');
+Route::post('/projects/confirm', 'ProjectsController@confirmStore');
+
+Route::get('/projects/done', 'ProjectsController@done');
