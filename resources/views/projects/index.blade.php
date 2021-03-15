e5b32d4d6f76dfa2787a4eb1a2f358cf0d2742a5
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-03-15 16:07:50 +0900

    案件一覧画面の作成

---
+++ resources/views/projects/index.blade.php
@@ -0,0 +1,18 @@
+<div>
+  <a href="{{ url('projects/create') }}">新規案件の登録</a>  
+  <h1>登録案件一覧</h1>
+  <table>
+    <tr>
+      <th>案件名</th>
+      <th>管理グループ</th>
+      <th>案件概要</th>
+    </tr>
+    @foreach ($projects as $project)
+      <tr>
+        <td>{{ $project->project_name }}</td>
+        <td>{{ $project->group->group_name }}</td>
+        <td>{{ $project->outline }}<td>
+      </tr>
+    @endforeach
+  </table>
+</div>
