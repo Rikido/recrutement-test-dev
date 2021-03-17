545733b75754e27dd998ba65779a1524970bd732
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-03-17 19:46:08 +0900

    crate_resourceのフォームリクエストの作成

---
+++ app/Http/Requests/CreateProjectResources.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateProjectResources extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'project_resource.*.resource' => 'required_with:project_resource.*.resource,project_resource.*.consumption_quantity|nullable|integer',
+            'project_resource.*.consumption_quantity' => 'required_with:project_resource.*.resource,project_resource.*.consumption_quantity|nullable|integer',
+        ];
+    }
+}
