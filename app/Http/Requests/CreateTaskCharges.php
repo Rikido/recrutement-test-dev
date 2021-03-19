b5464280c3a2ca8b8a5a0eb30edf8d9ba1fc0f7e
mao3329 <74639010+mao3329@users.noreply.github.com>
2021-03-19 13:55:50 +0900

    担当割り当て画面のフォームリクエストの追加

---
+++ app/Http/Requests/CreateTaskCharges.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CreateTaskCharges extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'task_charge.*.task_name' => 'required_with:task_charge.*.user,task_charge.*.outline,task_charge.*.order|nullable|string',
+            'task_charge.*.user' => 'required_with:task_charge.*.task_name,task_charge.*.outline,task_charge.*.order|nullable|integer',
+            'task_charge.*.outline' => 'required_with:task_charge.*.task_name,task_charge.*.user,task_charge.*.order|nullable|string',
+            'task_charge.*.order' => 'required_with:task_charge.*.task_name,task_charge.*.user,task_charge.*.outline|nullable|integer',
+        ];
+    }
+}
