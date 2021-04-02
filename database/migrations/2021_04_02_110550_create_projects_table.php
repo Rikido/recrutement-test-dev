f10003e4f17853c0fa7f816a71ad0b045f582b62
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-04-02 17:41:25 +0900

    groups、projectsのマイグレーション・モデル作成

---
+++ database/migrations/2021_04_02_110550_create_projects_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('project_name', 50);
+            $table->unsignedBigInteger('group_id');
+            $table->string('outline');
+            $table->string('file_path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
