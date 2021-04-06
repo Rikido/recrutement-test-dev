c14ed10ac06e1bf428add64ce043bc6a6d017235
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-04-06 16:43:11 +0900

    案件作成画面と案件作成処理、ビューとコントローラーの記述

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,42 +3,105 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
 use App\Project;
+// Validatorファザードの使用
+use Validator;
 
 class ProjectsController extends Controller
 {
-    public function index() {
+    // ログインしていない場合はログインページへ移動
+    public function __construct(){
+        $this->middleware('auth');
+    }
+
+    // 登録データ([フォーム名])
+    private $projectContents = ['project_name', 'group_id', 'outline', 'file_path'];
+
+    // バリデーション情報
+    private $validator = [
+        // requiredで入力必須
+        'project_name' => 'required|string|max:100',
+        'group_id' => 'required|integer',
+        'outline' => 'required|string|max:255',
+        // mimesでファイルが指定された拡張子かどうか判定する
+        'file_path' => 'required|file|mimes:pdf',
+    ];
+
+    public function index()
+    {
         $projects = Project::all();
 
         return view('projects.index');
     }
 
-    public function show() {
+    public function create()
+    {
+        // ログインユーザーが所属しているgroupを全て取得
+        $auth_user_groups = auth()->user()->groups;
+
+        return view('projects.create', ['auth_user_groups' => $auth_user_groups]);
+    }
+
+    public function post(Request $request)
+    {
+        // 入力データの取得
+        $input = $request->only($this->projectContents);
+        // バリデータの生成（makeメソッドの第１引数にバリデーションを行うデータ、第２引数にそのデータに適用するバリデーションルール）
+        $validator = Validator::make($input, $this->validator);
+        // エラー時の処理
+        if($validator->fails()){
+            // 案件作成画面にリダイレクト
+            return redirect()->action('ProjectsController@create')
+            // セッション(_old_input)に入力値を全て格納
+            ->withInput()
+            // セッション(errors)にエラーの情報を格納
+            ->withErrors($validator);
+        }
+        // セッションへデータを保存
+        $request->session()->put('project_input', $input);
+        // 作成案件確認画面に遷移
+        return redirect()->action('ProjectsController@confirm');
+    }
+
+    public function comfirm(Request $request)
+    {
+        // セッションから値を取り出す
+        $input = $request->session()->get('project_input');
+        // セッションに値が無い時はフォームに戻る
+        if(!$input){
+            return redirect()->action('ProjectsController@create');
+        }
+        return view('projects.confirm',['input' => $input]);
+    }
+
+    public function register(Request $request)
+    {
+        $input = $request->session()->get('project_input');
+
+        if(!$input){
+            return redirect()->action('ProjectsController@create');
+        }
+        // projectの登録処理
+        $project = new Project;
+        $project->project_name = $input['project_name'];
+        $project->group_id = $input['group_id'];
+        $project->outline = $input['outline'];
+        $project->file_path = $input['file_path'];
+        $project->save();
+        // セッションを空にする、セッションから値を削除するときはforget()関数を使用
+        $request->session()->forget('project_input');
+
+        return redirect()->action('ProjectsController@complete');
+    }
+
+    public function complete()
+    {
+        return view('projects.complete');
+    }
+
+    public function show()
+    {
 
         return view('projects.show');
-    }
-
-    public function create() {
-
-        return view('projects.create');
-    }
-
-    public function post() {
-
-    }
-
-    public function comfirm() {
-
-        return view('projects.comfirm');
-    }
-
-    public function register() {
-
-    }
-
-    public function complete() {
-
-        return view('projects.complete');
     }
 }
--- resources/views/projects/create.blade.php
+++ resources/views/projects/create.blade.php
@@ -3,3 +3,49 @@
 	案件概要と設計書データを登録、<br>
 	所有グループの登録をする
 </p>
+
+<h3>入力フォーム</h3>
+{{-- バリデーションエラーの表示 --}}
+@if ($errors->any())
+	<div style="color:red;">
+    	<ul>
+    		@foreach ($errors->all() as $error)
+    			<li>{{ $error }}</li>
+    		@endforeach
+    	</ul>
+    </div>
+@endif
+{{-- ファイルを送信するためform要素にenctype="multipart/form-data"を記述 --}}
+<form method="post" action="{{ url('project/create') }}" enctype="multipart/form-data">
+	@csrf
+
+	<label>案件名</label>
+	<div>
+		{{-- バリデーションエラーで画面が戻った際、 old()関数を使用して入力値を表示する --}}
+		<input type="text" name="project_name" value="{{ old('project_name') }}" />
+	</div>
+	<label>案件概要</label>
+	<div>
+		<textarea name="outline">{{ old('outline') }}</textarea>
+	</div>
+	<label>設計書pdfファイル登録</label>
+	<div>
+		<input type="file" accept=".pdf" name="file_path" />
+	</div>
+	<label>担当グループ選択</label>
+	<div>
+		<select name="group_id">
+			@foreach($auth_user_groups as $auth_user_group)
+				{{-- selectのname属性の値をold()の引数に指定 --}}
+				{{-- if分でidの値が「１」なら@if(old('id')=='1') selecte @endif がselectedされる --}}
+				<option value="{{ $auth_user_group->id }}" @if(old("group_id") == $auth_user_group->id) selected @endif>
+					{{ $auth_user_group->group_name }}
+				</option>
+			@endforeach
+		</select>
+	</div>
+
+	<input type="submit" value="作成案件確認画面へ" />
+</form>
+
+<a href="{{ url('/') }}">案件一覧へ戻る</a>
