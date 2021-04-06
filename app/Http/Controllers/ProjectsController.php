c9757b2244f108847127e1177294aeafb14d985c
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-04-06 10:11:10 +0900

    projectsのルーティング記述、ビューファイル作成

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -13,4 +13,32 @@ class ProjectsController extends Controller
 
         return view('projects.index');
     }
+
+    public function show() {
+
+        return view('projects.show');
+    }
+
+    public function create() {
+
+        return view('projects.create');
+    }
+
+    public function post() {
+
+    }
+
+    public function comfirm() {
+
+        return view('projects.comfirm');
+    }
+
+    public function register() {
+
+    }
+
+    public function complete() {
+
+        return view('projects.complete');
+    }
 }
