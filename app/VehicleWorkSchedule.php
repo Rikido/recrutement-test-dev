2cd003aa0eb6e82eaa3f1dd1c91e55c1fd182b49
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-04-08 11:06:27 +0900

    VehicleWorkScheduleモデルのリレーション

--- app/VehicleWorkSchedule.php
+++ app/VehicleWorkSchedule.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class VehicleWorkSchedule extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function project()
+    {
+        return $this->belongsTo('App\Project');
+    }
+
+    public function vehicle()
+    {
+        return $this->belongsTo('App\Vehicle');
+    }
 }
