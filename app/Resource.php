51656d170ce882b9deef835e216fe7005004d5b0
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-04-08 10:39:13 +0900

    Resourceモデルにリレーション記述

--- app/Resource.php
+++ app/Resource.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Resource extends Model
 {
-    //
+    public function resource_stocks()
+    {
+        return $this->hasMany('App\ResourceStock');
+    }
+
+    public function project_resources()
+    {
+        return $this->hasMany('App\ProjectResource');
+    }
 }
