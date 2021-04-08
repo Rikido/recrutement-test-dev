2e81b3c95fb75e03d5731bd7314ba78ecbb22d83
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-04-08 11:04:14 +0900

    ProjectResourceモデルのリレーション

--- app/ProjectResource.php
+++ app/ProjectResource.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectResource extends Model
 {
-    //
+    public function resource()
+    {
+        return $this->belongsTo('App\Resource');
+    }
+
+    public function project()
+    {
+        return $this->belongsTo('App\Project');
+    }
+
+    public function location()
+    {
+        return $this->belongsTo('App\Location');
+    }
 }
