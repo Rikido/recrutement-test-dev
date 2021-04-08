96e145ceb0c65f16c6432fe50a437c2ec8886967
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-04-08 10:48:53 +0900

    ResourceStockモデルのリレーション

--- app/ResourceStock.php
+++ app/ResourceStock.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class ResourceStock extends Model
 {
-    //
+    public function resource()
+    {
+        return $this->belongsTo('App\Resource');
+    }
+
+    public function location()
+    {
+        return $this->belongsTo('App\Location');
+    }
 }
