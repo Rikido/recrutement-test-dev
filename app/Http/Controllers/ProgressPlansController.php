a0fdd7ddebc36f47446e8fb9eae19dd905cfc9da
Yasuaki Yoshida <yasu5293koh@gmail.com>
2021-04-11 10:10:43 +0900

    大型資材積込み拠点選択画面の作成

--- app/Http/Controllers/ProgressPlansController.php
+++ app/Http/Controllers/ProgressPlansController.php
@@ -51,7 +51,7 @@ class ProgressPlansController extends Controller
 //         dd($project_resource);
 
         // セッションへデータを保存
-        $request->session()->put(['project' => $project, 'project_resource_input' => $project_resource]);
+        $request->session()->put('project_resource_input', $project_resource);
         // 担当割当画面に遷移
         return redirect()->action('ProgressPlansController@task_charge', ['id' => $project->id]);
     }
@@ -99,8 +99,7 @@ class ProgressPlansController extends Controller
         $project_resources = $request->session()->get('project_resource_input');
 //         dd($project_resources);
         // 利用資材入力画面で選択した資材に大型資材が含まれているか確認
-        foreach( $project_resources as $key => $project_resource)
-        {
+        foreach($project_resources as $key => $project_resource) {
             // project_resourcesのresource_idを取得
             $resource_id = $project_resource["resource_name"];
             // 資材マスタデータの取得
@@ -116,9 +115,69 @@ class ProgressPlansController extends Controller
     }
 
     // 大型資材積込み拠点選択画面
-    public function location($id) {
+    public function location($id, Request $request) {
         $project = Project::findOrFail($id);
-        return view('progress_plans.location', compact('project'));
+        // 大型資材在庫を全て取得する
+        $resource_stocks = ResourceStock::all();
+        // 利用資材入力画面でセッションに保存した値を取り出す
+        $project_resources = $request->session()->get('project_resource_input');
+//         dd($project_resources);
+        // 利用資材入力画面で選択した大型資材を格納する配列
+        $large_resource_array = [];
+        // 利用資材入力画面で選択した資材に大型資材が含まれているか確認
+        foreach((array)$project_resources as $key => $project_resource) {
+            $resource_id = $project_resource["resource_name"];
+            $resource = Resource::find($resource_id);
+            if($resource->resource_type == true) {
+                // true(大型資材)の資材マスタデータのみ配列に格納する
+                array_push($large_resource_array, $resource);
+            }
+        };
+//         dd($large_resource_array);
+        $location_select_array = [];
+        $index = 0;
+        foreach($large_resource_array as $large_resource) {
+            // 利用資材入力画面で選択された資材マスタのidに一致するresource_stocksを全て取得する
+            $large_resource_stocks = ResourceStock::where('resource_id', $large_resource["id"])->get();
+            // [["location" => ID, "resource" => ID, "consumption_quantity" => 使用数], [], []...]の形式で値を格納
+//             dd($large_resource_stocks);
+            $stock_check = 0;
+            foreach($large_resource_stocks as $key => $large_resource_stock) {
+                // 大型資材の在庫数を格納
+                $stock = $large_resource_stock['stock'];
+                // 最も在庫数が多い大型資材在庫のデータを格納する処理
+                if($stock_check < $stock) {
+                    $stock_check = $stock;
+                    // 在庫数が一番多いレコードの拠点IDを追加
+                    $location_select_array[$index]["location"] = $large_resource_stock["location_id"];
+                    // 在庫数が一番多いレコードの資材IDを追加
+                    $location_select_array[$index]["resource"] = $large_resource_stock["resource_id"];
+                    // 使用数を取得
+                    foreach((array)$project_resources as $key => $project_resource) {
+                        if($project_resource["resource_name"] == $large_resource["id"]) {
+                            // 使用数を追加
+                            $location_select_array[$index]["consumption_quantity"] = $project_resource["consumption_quantity"];
+                        }
+                    };
+//                     // もし在庫足りない場合の処理（在庫数 - 使用数）
+//                     if(0 > ($stock_check - $location_select_array[$index]["consumption_quantity"])) {
+//                         // 必要な残りの使用数（使用数 - 在庫数）
+//                         $next_consumption_quantity = $location_select_array[$index]["consumption_quantity"] - $stock_check;
+//                         // 格納した使用数を在庫数を考慮した値に修正（在庫全て）
+//                         $location_select_array[$index]["consumption_quantity"] = $stock_check;
+//                         // 次の拠点を格納
+//                         $index++;
+//                         $location_select_array[$index]["location"] =
+//                         $location_select_array[$index]["resource"] =
+//                         $location_select_array[$index]["consumption_quantity"] = $next_consumption_quantity;
+//                     }
+                }
+            }
+            $index++;
+        }
+//         dd($location_select_array);
+//         dd($large_resource_array);
+        return view('progress_plans.location', compact('project', 'resource_stocks', 'large_resource_array', 'location_select_array'));
     }
 
     // 大型資材積込み拠点選択画面の入力値をセッションに登録する処理
--- resources/views/progress_plans/location.blade.php
+++ resources/views/progress_plans/location.blade.php
@@ -5,3 +5,38 @@
 	また、全貯蔵拠点の一覧と各在庫を表示し、システムが提案する積込みに対する、任意の積込み計画を設定可能にする。<br>
 	・予めシステムで算出した積込み数を入力済み状態で表示する。
 </p>
+
+<h3>貯蔵拠点一覧及び各資材の在庫数、大型資材積み込み数選択</h3>
+<form method="post" action="/project/{{ $project->id }}/progress_plan/location">
+	@csrf
+
+	@foreach ($resource_stocks as $resource_stock)
+		<label>拠点名</label>
+    	<div>
+    		{{ $resource_stock->location->location_name }}
+    	</div>
+    	<label>資材名</label>
+    	<div>
+    		 {{ $resource_stock->resource->resource_name  }}
+    	</div>
+    	<label>在庫数</label>
+    	<div>
+    		{{ $resource_stock->stock }}
+    	</div>
+    	<label for="consumption_quantity">積み込み数</label>
+    	<div>
+    		<input type="number" min="1" name="consumption_quantity" value=
+        		@foreach ($location_select_array as $key => $location_select)
+        			@if(($location_select["location"] == $resource_stock->location_id) && ($location_select["resource"] == $resource_stock->resource_id))
+        				{{ old("consumption_quantity", $location_select["consumption_quantity"]) }}
+        			@endif
+        		@endforeach
+    		 >
+    	</div>
+	@endforeach
+	{{-- 使用数が入力されていない配列は削除する --}}
+
+	<input name="back" type="button" onClick="history.back()" value="修正する" />
+	<input type="submit" value="次の画面へ進む">
+</form>
+
