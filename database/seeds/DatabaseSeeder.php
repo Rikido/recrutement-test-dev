2db3d1425608f3589bbd48cd2c2f543fabc0afb5
大澤実紀 <oosawaminori@oosawamikinoMacBook-Pro.local>
2022-06-11 11:45:44 +0900

    groupのfactoryファイル、seederファイルの記述

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        //user登録
+        factory(App\User::class, 30)->create();
+
+        //groupにuserを所属させる
+        $users = App\User::all();
+
+        factory(App\Group::class, 5)
+          ->create()
+          ->each(function ($group) use ($users) {
+              //中間テーブルに紐付け
+              $group->users()->attach(
+                  //3~5個のuserをgroupにランダムに紐付け
+                  $users->random(rand(3,5))->pluck('id')->toArray()
+              );
+          });
     }
 }
