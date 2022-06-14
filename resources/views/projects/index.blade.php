6469ba50e1d06adcac711d0542110ce02e8b496b
大澤実紀 <oosawaminori@oosawamikinoMacBook-Pro.local>
2022-06-14 21:11:39 +0900

    案件一覧画面作成

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -3,9 +3,27 @@
 @section('content')
 <div class = container>
   <div>
-    <h1>案件一覧</h1>
+    <div class="d-flex justify-content-end">
+      <a href="{{ url('projects/create') }}" class="btn btn-primary">新規案件の登録</a>
+    </div>
+    <h2>案件一覧</h2>
     @foreach ($projects as $project)
-
+    <table class="table">
+      <thead>
+        <tr>
+          <th>案件名</th>
+          <th>案件概要</th>
+          <th>担当グループ</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th><a href="{{ url('project', $project->id) }}">{{ $project->project_name }}</a></th>
+          <td>{{ $project->outline }}</td>
+          <td>{{ $project->group->group_name }}</td>
+        </tr>
+      </tbody>
+    </table>
     @endforeach
   </div>
 </div>
